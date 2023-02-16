02c17b02793bea8056d8d032b24edd6c6936c016
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2023-02-16 23:18:42 +0530

    gallety

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import './App.css';
 import NavBar from './components/NavBar';
-import { Gallery } from "./components/gallery";
+import { Gallery } from "./components/Gallery";
+import {
+  BrowserRouter as Router,
+
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+export class Gallery extends Component{
+    render(){
+        return(
+            <div>
+                "My Name is Khan"
+            </div>
+        )
+    }
+
+}
