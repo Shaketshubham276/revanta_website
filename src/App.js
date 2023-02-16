9cfe6b3723ced437682c44a89d5bdf4ed4cf43db
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2023-02-16 23:18:22 +0530

    done gallery.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import './App.css';
 import NavBar from './components/NavBar';
+import { Gallery } from "./components/gallery";
 
 function App() {
   return (
     <>
+    <Router>
       <NavBar/>
+      <div className="container">
+        <Routes>
+          
+          <Route exact path="/about" element={<Gallery/>}></Route>
+
+        </Routes>
+      </div>
+    </Router>
+      
     </>
   );
 }
