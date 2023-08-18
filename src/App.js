f0839db5130afc7b6c83e18b6f3a3722f41bfb9b
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2023-08-18 21:52:00 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import React from "react";
-import './App.css';
-import NavBar from './components/js/NavBar';
-import { Gallery } from "./components/js/Gallery";
-
-import {
-  BrowserRouter as Router,
-
-  Route,
-  Routes
-} from "react-router-dom";
 
 function App() {
   return (
-    <>
-    {/* <Router> */}
-      <NavBar/>
-      <Gallery/>
-      <samosa/>
-      {/* <div className="container">
-        <Routes>
-          
-          <Route exact path="/gallery" element={<Gallery/>}></Route>
-
-        </Routes> */}
-      {/* </div> */}
-    {/* // </Router> */}
-      
-    </>
+    <h1>Whatsup</h1>
   );
 }
 
