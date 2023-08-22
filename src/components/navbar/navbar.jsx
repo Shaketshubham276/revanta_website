e5059155d6bf9bdccd9b17daf1027a8034e6dcd4
unknown <abhiram050904@gmail.com>
2023-08-22 13:02:37 +0530

    hamburger navabar with about us

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,34 +1,30 @@
 import React from 'react'
 import './navbar.css'
-const navbar = () => {
+
+const Navbar = () => {
+  const onclickMenu = () => {
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+  };
+
   return (
     <div>
-      <div className="navbar">
-  <div className="list">
-    <ul>
-      <img
-        className="image"
-        src="./326480590_1591450784610208_3385628294328587499_n.jpg"
-        alt="image" />
-      <li>home</li>
-      <li>about</li>
-      <li>content</li>
-      <li> database</li>
-      <li>achievements</li>
-      <li>discovery</li>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search the website"
-          className="search-box"
-        />
+      <div id="navigation">
+        <div id="menu" onClick={onclickMenu}>
+          <div id="bar1" className="bar"></div>
+          <div id="bar2" className="bar"></div>
+          <div id="bar3" className="bar"></div>
+        </div>
+        <ul className="nav" id="nav">
+          <li><a href="#">home</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">blog</a></li>
+          <li><a href="#">gallery</a></li>
+          <li><a href="#">contact</a></li>
+        </ul>
       </div>
-    </ul>
-  </div>
-</div>
-
     </div>
   )
 }
 
-export default navbar
+export default Navbar;
